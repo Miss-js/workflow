cabc79a92fdda83072b05327e1890e719daa8b8b
mom-of-charles <14082019.qq.com>
2025-12-13 19:33:17 +0800

    add a h2 tag in app component

--- app/root.tsx
+++ app/root.tsx
@@ -33,7 +33,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Links />
       </head>
       <body>
-        <h2>f2's</h2>
+        <h2>f2's react router project</h2>
         {children}
         <ScrollRestoration />
         <Scripts />
